9ef4b76f9ad83f86dbe8af611893ad79722d112e
berkaygulsen <berkaygulsen@gmail.com>
2022-10-23 22:02:57 +0300

    building the layout

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Container = styled.div`
+    flex: 1;
+    margin: 3px;
+    height: 70vh;
+    position: relative;
+`
+
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
+
+const Info = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    opacity: 1;
+    background-color: rgba(0,0,0,0.2);
+    transition: all 0.5s ease;
+    cursor: pointer;
+    z-index: 2;
+`
+
+
+const Title = styled.h1`
+    color: white;
+    margin-bottom: 20px;
+`
+const Desc = styled.p``
+
+const Button = styled.button`
+    border: 0.5px solid white;
+    padding: 15px;
+    background-color: transparent;
+    cursor: pointer;
+    font-weight: 600;
+    color: white;
+    transition: all 0.5s ease;
+    &:hover {
+        background-color: rgba(255,255,255,0.5);
+    }
+`
+
+const CategoryItem = ({ item }) => {
+    return (
+        <Container>
+            <Image src={item.img} />
+            <Info>
+                <Title>{item.title}</Title>
+                <Desc>{item.desc}</Desc>
+                <Button>SHOW NOW</Button>
+            </Info>
+
+        </Container>
+    );
+};
+
+export default CategoryItem;
---
+++ src/components/Slider.jsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import styled from "styled-components";
+import {ArrowLeft, ArrowRight} from "@mui/icons-material";
+import {sliderItems} from "../data";
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: #f5fbfd;
+    display: flex;
+    position: relative;
+`
+
+const Arrow = styled.div`
+    width: 50px;
+    height: 50px;
+    background-color: #fff;
+    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: ${props => props.direction === "left" && "10px"};
+    right: ${props => props.direction === "right" && "10px"};
+    margin: auto;
+    opacity: 0.5;
+    z-index: 2;
+`
+
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    transform: translateX(${props => props.slideIndex * -100}vw);
+    transition: all 1.5s ease;
+`
+
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    background-color: #${props => props.bg};
+`
+
+const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`
+
+const Image = styled.img`
+    height: 80%;
+`
+
+const Title = styled.h1`
+    font-size: 70px;
+`
+
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    cursor: pointer;
+    background-color: transparent;
+`
+
+
+const Slider = () => {
+
+    const [slideIndex, setSlideIndex] = React.useState(0);
+
+    const handleClick = (direction) => {
+        if (direction === "left") {
+            setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2)
+        } else {
+            setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0)
+        }
+    }
+
+    return (
+        <Container>
+            <Arrow direction="left" onClick={() => handleClick("left")}>
+                <ArrowLeft />
+            </Arrow>
+            <Wrapper slideIndex={slideIndex}>
+                {sliderItems.map(item => (
+                    <Slide bg={item.bg} key={item.id}>
+                        <ImgContainer>
+                            <Image src={item.img} />
+                        </ImgContainer>
+                        <InfoContainer>
+                            <Title>{item.title}</Title>
+                            <Desc>{item.desc}</Desc>
+                            <Button>SHOP NOW</Button>
+                        </InfoContainer>
+                    </Slide>
+                ))}
+            </Wrapper>
+            <Arrow direction="right" onClick={() => handleClick("right")}>
+                <ArrowRight />
+            </Arrow>
+        </Container>
+    );
+};
+
+export default Slider;
